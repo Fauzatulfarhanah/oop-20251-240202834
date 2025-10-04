c9bb75a18a296c5b410f95608b3b3b294cb3949a
Fauzatulfarhanah <fauzatulfarhanah960@gmail.com>
2025-10-04 07:37:13 +0700

    HelloFunctional.java

---
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloFunctional.java
@@ -0,0 +1,11 @@
+import java.util.function.BiConsumer;
+
+public class HelloFunctional {
+    public static void main(String[] args) {
+        // Paradigma fungsional: pakai fungsi murni (lambda)
+        BiConsumer<String, String> sayHello = (name, nim) -> 
+            System.out.println("Hello World, I am " + name + "-" + nim);
+
+        sayHello.accept("Fauzatul Farhanah", "240202834");
+    }
+}
