de090f0a3ba923d4d51bc8f31202abb37270ac7d
Fauzatulfarhanah <fauzatulfarhanah960@gmail.com>
2025-10-04 07:05:55 +0700

    Move: Tambahkan HelloOOP, HelloProcedural, HelloFunctional ke folder src/main/java/com/upb/agripos/

---
+++ praktikum/week1-setup-hello-pos/HelloProcedural.java
@@ -0,0 +1,8 @@
+public class HelloProcedural {
+    public static void main(String[] args) {
+        // Paradigma prosedural: langsung instruksi urut
+        String nama = "Fauzatul Farhanah";
+        String nim = "240202834";
+        System.out.println("Hello World, I am " + nama + "-" + nim);
+    }
+}
